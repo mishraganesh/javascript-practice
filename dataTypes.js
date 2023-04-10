d6a0ee2fc23adfc610fe75d1d9ba07ed93683f8e
ganesh <mishraganesh051@gmail.com>
2023-04-10 11:13:28 +0530

    data type

---
+++ dataTypes.js
@@ -0,0 +1,40 @@
+//data types in javascript 
+//js use dyanamic type of data type 
+//total 9 data type available in js
+
+//primitive data type -means you can only store single value using this type like,number,string,boolean
+//1.number
+let age=25;
+let price=25.62;
+let tmp=-52;
+
+//2.string
+let name="ganesh"
+//3.boolean use two value
+let isLoggedIn=true;
+let logout=false
+
+//4.undefined-means you define the variable but still you did not assign any value.
+let name1;
+console.log(name1)
+
+//5. bigint-use for large number operation like power
+//6. symbol
+
+
+
+//2.structural data type
+//1.objects
+  //1.1 function -this also called non-struture
+  //this is callable and reusable
+  //1.2 Array -is a collection of values
+  //array is a object
+  const arr=[2,3,4,"javascript",{name:"ganesh"}];
+  //1.3 maps
+  //1.4 set for use check uniqueness
+  //1.5 date 
+
+  //3.structural root
+   //null
+
+
